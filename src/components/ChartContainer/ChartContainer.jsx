e6b9ac791b7585528bdb48aea31deb15514656cf
KlausR98 <szymoon_rodak@o2.pl>
2024-01-25 13:07:47 +0100

    first commit

---
+++ src/components/ChartContainer/ChartContainer.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+const ChartContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChartContainer
---
+++ src/components/SmallChartContainer/SmallChartContainer.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+const SmallChartContainer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SmallChartContainer
